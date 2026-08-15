82af20959c0d5e41481eb11f60249cae15845d84
andreyfesunov <117005912+andreyfesunov@users.noreply.github.com>
2026-08-15 21:12:56 +0300

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.go
@@ -0,0 +1,20 @@
+func searchMatrix(matrix [][]int, target int) bool {
+    m := len(matrix)
+    n := len(matrix[0])
+    l, r := 0, m * n - 1
+
+    for l <= r {
+        mid := l + (r - l) / 2
+        mmid, nmid := mid / n, mid % n
+
+        if matrix[mmid][nmid] > target {
+            r = mid - 1
+        } else if matrix[mmid][nmid] < target {
+            l = mid + 1
+        } else {
+            return true
+        }
+    }
+
+    return false
+}
