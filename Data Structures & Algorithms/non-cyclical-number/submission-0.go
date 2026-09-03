f14d4a9113bd87a1324ce5b38a7166c80285ffea
andreyfesunov <117005912+andreyfesunov@users.noreply.github.com>
2026-09-03 16:21:01 +0300

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.go
@@ -0,0 +1,23 @@
+func isHappy(n int) bool {
+    visit := make(map[int]bool)
+
+    for !visit[n] {
+        visit[n] = true
+        n = sumOfSquares(n)
+        if n == 1 {
+            return true
+        }
+    }
+    return false
+}
+
+func sumOfSquares(n int) int {
+    output := 0
+    for n > 0 {
+        digit := n % 10
+        digit = digit * digit
+        output += digit
+        n = n / 10
+    }
+    return output
+}
