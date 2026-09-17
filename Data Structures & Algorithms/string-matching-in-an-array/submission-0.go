c563239c1fe27d29e022ed4519b252b1f2c0c024
andreyfesunov <117005912+andreyfesunov@users.noreply.github.com>
2026-09-17 23:31:09 +0300

    Add: string-matching-in-an-array - submission-0

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-0.go
@@ -0,0 +1,17 @@
+func stringMatching(words []string) []string {
+    res := []string{}
+    sort.Slice(words, func(i, j int) bool {
+        return len(words[i]) < len(words[j])
+    })
+
+    for i := 0; i < len(words); i++ {
+        for j := i + 1; j < len(words); j++ {
+            if strings.Contains(words[j], words[i]) {
+                res = append(res, words[i])
+                break
+            }
+        }
+    }
+
+    return res
+}
